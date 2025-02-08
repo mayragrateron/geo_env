6d4823e0dc91967dda230118474137de8e3d95a7
mayragrateron <99696693+mayragrateron@users.noreply.github.com>
2025-02-08 11:52:28 +0300

    Add files via upload

---
+++ assignment_2.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pdb
+import xarray as xr
+dset = xr.open_dataset(r'C:/Users/mayra/geo_env/Course_Data/Climate_Model_Data/tas_Amon_GFDL-ESM4_historical_r1i1p1f1_gr1_195001-201412.nc')
+pdb.set_trace()
